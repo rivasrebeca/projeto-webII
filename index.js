af870b932d34f87b7bc3b621d04baff9441b1510
rivasrebeca <rivas.rebeca@ifce.edu.br>
2025-09-18 19:45:25 +0000

    adiciona o nodemon

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.listen(3001, () =>{
-    console.log("Servidor rodando na porta 3001 e eu estou feliz");
+    console.log("Servidor rodando na porta 3001");
 })
