83970205ffc0d17a169ddb8c61e7225f1d7b62d1
rebecarivas <rebecarivas@gmail.com>
2025-10-07 16:46:12 -0300

    adiciona rota, tabela, e endpoints para Comments e faz associação com a tabela Posts

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const db = require('./models');
 const postRouter = require('./routes/Posts');
 app.use("/posts", postRouter);
 
+const commentRouter = require('./routes/Comments');
+app.use("/comments", commentRouter);
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () =>{
     console.log("Servidor rodando na porta 3001");
